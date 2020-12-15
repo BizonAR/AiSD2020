0c1e06de7ceb1a7c5e03b5352ba5962928ff4d67
Andrey Rechkalov <61356488+BizonAR@users.noreply.github.com>
2020-12-15 11:41:12 +0500

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import openpyxl
 
 def last_day_of_month(any_day):
     next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
@@ -29,7 +30,7 @@ midle_temperature_month = 0
 f = open('Temperature.txt', 'a+')
 f.write(day + ' of ' + month + ': ' + str(midle_temperature) + '\n')
 if (count_day_month == day):
-  count = 1
+  count = 0
   f.seek(0)
   if i in range(len(f)):
     count += 1
@@ -38,3 +39,18 @@ if (count_day_month == day):
   midle_temperature_month = midle_temperature_month / count
   f.write(midle_temperature_month)
 f.close()
+
+wb = openpyxl.load_workbook('Temperature.xlsx')
+sheet = wb['Лист1']
+if sheet.cell(row = 1, column=1).value == None:
+  sheet.cell(row = 1, column=1).value = "Date"
+  sheet.cell(row = 1, column=2).value = "Average daily temperature in °С"
+row_count = sheet.max_row
+sheet.insert_rows(idx = row_count, amount = 1)
+sheet.cell(row = row_count, column = 1).value = day + ' of ' + month
+sheet.cell(row =row_count, column = 1).value = str(midle_temperature)
+if (count_day_month == day):
+  row_count = sheet.max_row
+  sheet.insert_rows(idx = row_count, amount = 1)
+  sheet.cell(row = row_count, column = 1).value = 'average monthly temperature: '
+  sheet.cell(row = row_count, column = 2).value = midle_temperature_month
